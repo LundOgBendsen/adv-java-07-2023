f6552e1a87965cedd0a3fb18862b7aa9a58049cf
jbendsen <jakob@logb.dk>
2023-07-06 18:44:04 +0200

    after module 1, switched to zxing, removed maven repo

--- generator/src/main/java/dk/logb/qr/generator/controller/BarcodeController.java
+++ generator/src/main/java/dk/logb/qr/generator/controller/BarcodeController.java
@@ -1,6 +1,10 @@
 package dk.logb.qr.generator.controller;
 
-import net.glxn.qrgen.javase.QRCode;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.EAN13Writer;
+import com.google.zxing.qrcode.QRCodeWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,24 +24,22 @@ import java.io.ByteArrayOutputStream;
 @RequestMapping("/barcodes")
 public class BarcodeController {
 
-        @GetMapping(value = "/code/{barcode}", produces = MediaType.IMAGE_PNG_VALUE)
-        public ResponseEntity<BufferedImage> barbecueEAN13Barcode(@PathVariable("barcode") String barcode)
-                throws Exception {
+    @GetMapping(value = "/code/{barcode}", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<BufferedImage> barbecueEAN13Barcode(@PathVariable("barcode") String barcode)
+            throws Exception {
 
-            return ResponseEntity.ok(generateQRCodeImage(barcode));
-        }
+        return ResponseEntity.ok(generateQRCodeImage(barcode));
+    }
+
     public static BufferedImage generateQRCodeImage(String barcodeText) throws Exception {
-        ByteArrayOutputStream stream = QRCode
-                .from(barcodeText)
-                .withSize(250, 250)
-                .stream();
-        ByteArrayInputStream bis = new ByteArrayInputStream(stream.toByteArray());
-        return ImageIO.read(bis);
+        QRCodeWriter barcodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 300, 300);
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
     }
 
     @Bean
     public HttpMessageConverter<BufferedImage> createImageHttpMessageConverter() {
         return new BufferedImageHttpMessageConverter();
     }
-    //...
+
 }
