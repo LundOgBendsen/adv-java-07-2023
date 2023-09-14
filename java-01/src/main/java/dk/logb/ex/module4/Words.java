d1dac85da0b26532fb317de7abf62a118f21e88a
jbendsen <jakob@logb.dk>
2023-09-14 21:13:15 +0200

    before m#4

--- java-01/src/main/java/dk/logb/ex/module4/Words.java
+++ java-01/src/main/java/dk/logb/ex/module4/Words.java
@@ -11,7 +11,13 @@ public class Words {
     public static void main(String[] args) throws IOException {
         List<String> allWords = getAllWords("https://www.ietf.org/rfc/rfc2616.txt");
 
-
+        //1. tæl antal ord
+        //2. tæl antal unikke ord
+        //3. find det længste ord
+        //4. sorter (alle) ordene alfabetisk
+        //5. sorter (unikke) ordene efter længde
+        //6. tæl hvor mange gange hvert ord forekommer (ord -> antal)
+        //7. udskriv forekomsterne, så de er sorteret efter antal, faldende
     }
 
 
